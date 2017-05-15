4959bd0c55b2c840ba02acd30691f2bc3e25f737
Joyounger <942510346@qq.com>
2017-05-15 21:07:17 +0800

    finish 1-1.c
    Signed-off-by: Joyounger <942510346@qq.com>

---
+++ chap1_UNIX_System_Overview/1-1.c
@@ -0,0 +1,25 @@
+// unix ls commmand simple version
+
+// date:17.4.17
+// author: linyang <942510346@qq.com>
+
+#include <apue.h>
+#include <dirent.h>
+
+int main(int argc, char const *argv[])
+{
+	DIR           *dp;
+	struct dirent *dirp;
+
+	if(argc != 2) {
+		err_quit("usage: ls directory_name");
+	}
+	if ((dp = opendir(argv[1])) == NULL) {
+		err_sys("can't open %s", argv[1]);
+	}
+	while(dirp = readdir(dp) != NULL) {
+		printf("%s\n", dirp -> d_name);
+	}
+
+	return 0;
+}
