5c45a5e43511d0be7398b5ea38e6e6e42f3d2db1
Joyounger <942510346@qq.com>
2017-05-17 00:34:52 +0800

    finish 1-8
    Signed-off-by: Joyounger <942510346@qq.com>

--- chap1_UNIX_System_Overview/1-8.c
+++ chap1_UNIX_System_Overview/1-8.c
@@ -13,4 +13,33 @@ int main(void)
 	pid_t pid;
 	int status;
 	
-	if (signal(SIGINT, sig_int) == SIG_ERR)
+	if (signal(SIGINT, sig_int) == SIG_ERR) {
+		err_sys("signal error");
+	}
+
+	printf("%% \n"); //print %
+	while (fgets(buf, MAXLINE, stdin) != NULL) {
+		if (buf[strlen(buf) - 1] == '\n') {
+			buf[strlen(buf) - 1] = 0; // replace newline with null
+		}
+
+		if ((pid = fork()) < 0) {
+			err_sys("fork error");
+		} else if (pid == 0) {
+			execlp(buf, buf, (char *)0);
+			err_ret("couldn't execute : %s", buf);
+			exit(127);
+		}
+
+		if ((pid = waitpid(pid, &status, 0)) < 0) {
+			err_sys("waitpid error");
+		}
+		printf("%% ");
+	}
+	exit(0);
+}
+
+void sig_int(int signo)
+{
+	printf("interrupt\n%% ");
+}
